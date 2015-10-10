1792a5f8eeec2a0151345792149af641f812163b
cdriscol <drizkol@gmail.com>
2015-10-10 15:12:51 -0600

    adding tweenmax to prod

--- config/assets/production.js
+++ config/assets/production.js
@@ -7,6 +7,7 @@ module.exports = {
         'public/lib/angular-material/angular-material.css',
       ],
       js: [
+        'public/lib/gsap/src/minified/TweenMax.min.js',
         'public/lib/angular/angular.min.js',
         'public/lib/angular-resource/angular-resource.min.js',
         'public/lib/angular-animate/angular-animate.min.js',
