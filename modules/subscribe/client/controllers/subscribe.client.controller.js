9088ac523b9332a855cf77384ebc3862cb3465c3
cdriscol <drizkol@gmail.com>
2015-10-10 14:21:11 -0600

    breaking subscribe into its own module

--- modules/subscribe/client/controllers/subscribe.client.controller.js
+++ modules/subscribe/client/controllers/subscribe.client.controller.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('core')
+angular.module('subscribe')
   .controller('SubscribeController', SubscribeController);
   
 SubscribeController.$inject = ['$scope', '$http', '$timeout'];
